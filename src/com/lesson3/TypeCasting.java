bdd2484263814baf891ba45da06279152971a336
Serhii Rudyi <rudyjj@gmail.com>
2020-04-20 12:02:25 +0300

    Add lesson 3

---
+++ src/com/lesson3/TypeCasting.java
@@ -0,0 +1,276 @@
+package com.lesson3;
+
+public class TypeCasting {
+
+    public static byte byteToByte(byte arg) {
+        return arg;
+    }
+
+    public static short byteToShort(byte arg) {
+        return arg;
+    }
+
+    public static char byteToChar(byte arg) {
+        return (char) arg;
+    }
+
+    public static int byteToInt(byte arg) {
+        return arg;
+    }
+
+    public static long byteToLong(byte arg) {
+        return arg;
+    }
+
+    public static float byteToFloat(byte arg) {
+        return arg;
+    }
+
+    public static double byteToDouble(byte arg) {
+        return arg;
+    }
+
+    /*
+    public static boolean byteToBoolean(byte arg) {
+        return arg;
+    }
+    */
+
+    public static byte shortToByte(short arg) {
+        return (byte) arg;
+    }
+
+    public static short shortToShort(short arg) {
+        return arg;
+    }
+
+    public static char shortToChar(short arg) {
+        return (char) arg;
+    }
+
+    public static int shortToInt(short arg) {
+        return arg;
+    }
+
+    public static long shortToLong(short arg) {
+        return arg;
+    }
+
+    public static float shortToFloat(short arg) {
+        return arg;
+    }
+
+    public static double shortToDouble(short arg) {
+        return arg;
+    }
+
+    /*
+    public static boolean short ToBoolean(byte arg) {
+        return arg;
+    }
+    */
+
+    public static byte charToByte(char arg) {
+        return (byte) arg;
+    }
+
+    public static short charToShort(char arg) {
+        return (short) arg;
+    }
+
+    public static char charToChar(char arg) {
+        return arg;
+    }
+
+    public static int charToInt(char arg) {
+        return arg;
+    }
+
+    public static long charToLong(char arg) {
+        return arg;
+    }
+
+    public static float charToFloat(char arg) {
+        return arg;
+    }
+
+    public static double charToDouble(char arg) {
+        return arg;
+    }
+
+    /*
+    public static boolean charToBoolean(char arg) {
+        return arg;
+    }
+    */
+
+    public static byte intToByte(int arg) {
+        return (byte) arg;
+    }
+
+    public static short intToShort(int arg) {
+        return (short) arg;
+    }
+
+    public static char intToChar(int arg) {
+        return (char) arg;
+    }
+
+    public static int intToInt(int arg) {
+        return arg;
+    }
+
+    public static long intToLong(int arg) {
+        return arg;
+    }
+
+    public static float intToFloat(int arg) {
+        return arg;
+    }
+
+    public static double intToDouble(int arg) {
+        return arg;
+    }
+
+    /*
+    public static boolean intToBoolean(int arg) {
+        return arg;
+    }
+    */
+
+    public static byte longToByte(long arg) {
+        return (byte) arg;
+    }
+
+    public static short longToShort(long arg) {
+        return (short) arg;
+    }
+
+    public static char longToChar(long arg) {
+        return (char) arg;
+    }
+
+    public static int longToInt(long arg) {
+        return (int) arg;
+    }
+
+    public static long longToLong(long arg) {
+        return arg;
+    }
+
+    public static float longToFloat(long arg) {
+        return arg;
+    }
+
+    public static double longToDouble(long arg) {
+        return arg;
+    }
+
+    /*
+    public static boolean longToBoolean(long arg) {
+        return arg;
+    }
+    */
+
+    public static byte floatToByte(float arg) {
+        return (byte) arg;
+    }
+
+    public static short floatToShort(float arg) {
+        return (short) arg;
+    }
+
+    public static char floatToChar(float arg) {
+        return (char) arg;
+    }
+
+    public static int floatToInt(float arg) {
+        return (int) arg;
+    }
+
+    public static long floatToLong(float arg) {
+        return (long) arg;
+    }
+
+    public static float floatToFloat(float arg) {
+        return arg;
+    }
+
+    public static double floatToDouble(float arg) {
+        return arg;
+    }
+
+    /*
+    public static boolean floatToBoolean(float arg) {
+        return arg;
+    }
+    */
+
+    public static byte doubleToByte(double arg) {
+        return (byte) arg;
+    }
+
+    public static short doubleToShort(double arg) {
+        return (short) arg;
+    }
+
+    public static char doubleToChar(double arg) {
+        return (char) arg;
+    }
+
+    public static int doubleToInt(double arg) {
+        return (int) arg;
+    }
+
+    public static long doubleToLong(double arg) {
+        return (long) arg;
+    }
+
+    public static float doubleToFloat(double arg) {
+        return (float) arg;
+    }
+
+    public static double doubleToDouble(double arg) {
+        return arg;
+    }
+
+    /*
+    public static boolean doubleToBoolean(double arg) {
+        return arg;
+    }
+    */
+
+    /*
+    public static byte booleanToByte(boolean arg) {
+        return arg;
+    }
+
+    public static short booleanToShort(boolean arg) {
+        return arg;
+    }
+
+    public static char booleanToChar(boolean arg) {
+        return arg;
+    }
+
+    public static int booleanToInt(boolean arg) {
+        return arg;
+    }
+
+    public static long booleanToLong(boolean arg) {
+        return arg;
+    }
+
+    public static float booleanToFloat(boolean arg) {
+        return arg;
+    }
+
+    public static double booleanToDouble(boolean arg) {
+        return arg;
+    }
+    */
+
+    public static boolean booleanToBoolean(boolean arg) {
+        return arg;
+    }
+}
